4312e76f21d9b4feee59b42750fc3a37a8dad7a9
Sergei Chertkov <chertkov.s.a@gmail.com>
2018-07-15 19:17:46 +0300

    Update

--- src/expected.hpp
+++ src/expected.hpp
@@ -4,30 +4,52 @@
 
 namespace hntr::platform {
 
-template<typename T>
-class expected {
-public:
-    constexpr explicit expected(T&& v) noexcept : _v(v) {}
-    constexpr explicit expected(int code, std::string_view) noexcept {}
+template<typename T, typename E> class expected;
 
-    operator bool() const { return true; }
-
-private:
-    T _v;
-};
-
-template<>
-class expected<void> {
-public:
-    constexpr explicit expected(int code, std::string_view) noexcept {}
-    operator bool() const { return true; }
-};
-
+template <typename E>
 class unexpected {
 public:
-    constexpr explicit unexpected(std::string_view) noexcept {}
-    constexpr operator bool() const { return false; }
+    unexpected(E&& e) : error(std::forward<E>(e)) {}
+    unexpected(E const& e) : error(e) {}
+
+    E error;
 };
+
+template<typename T, typename E>
+class expected {
+public:
+    explicit expected() : _val(T()) {}
+    explicit expected(T&& v) noexcept : _val(std::forward<T>(v)) {}
+
+
+    explicit expected(unexpected<E> const& e) noexcept 
+        : ok(false), _exc(e.error) {}
+
+    explicit expected(unexpected<E>&& e) noexcept 
+        : ok(false), _exc(std::move(e.error)) {}
+
+    operator bool() const { return ok; }
+
+    T& operator*() {
+        if (!ok) throw _exc;
+        return _val;
+    }
+
+    E& error() {
+        assert(!ok);
+        return _exc;
+    }
+
+    ~expected() {
+        if (ok) _val.~T();
+        else _exc.~E();
+    }
+
+private:
+    const bool ok = true;
+    union { T _val; E _exc; };
+};
+
 
 }
 
--- test/expected.cpp
+++ test/expected.cpp
@@ -5,12 +5,29 @@
 
 using namespace hntr::platform;
 
-TEST_CASE("Expected is expected", "[expected]") {
-    constexpr auto exp = expected(true);
+TEST_CASE("expected is expected", "[expected]") {
+    auto exp = expected<int, int>();
     REQUIRE(exp);
 }
 
-TEST_CASE("Unexpected is unexpected", "[expected]") {
-    constexpr auto unexp = unexpected("Something wrong");
-    REQUIRE(!unexp);
+TEST_CASE("expected value is correct", "[expected]") {
+    auto exp = expected<int, int>(123);
+    REQUIRE(exp);
+    REQUIRE(*exp == 123);
 }
+
+TEST_CASE("Unexpected is unexpected", "[expected]") {
+    auto unexp = expected<char, int>(unexpected(22));
+    REQUIRE(!unexp);
+    CHECK_THROWS_AS(*unexp, int);
+}
+
+TEST_CASE("Non-trivial types are ok", "[expected]") {
+    auto exp = expected<std::string, std::string>(std::string("aaa"));
+    REQUIRE(*exp == "aaa");
+
+    auto unexp = expected<int, std::string>(unexpected(std::string("bbb")));
+    REQUIRE(unexp.error() == "bbb");
+    CHECK_THROWS_AS(*unexp, std::string);
+}
+
